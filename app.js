12e6c2b8e529df3cce4218b9dbb552e049d9af3a
Sanjay Jaiswal <55450200+sanjay-xdr@users.noreply.github.com>
2021-12-18 19:56:41 +0530

    init

---
+++ app.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+const authRoutes = require("./routes/auth");
+const userRoutes=require("./routes/user");
+const categoryRoutes=require("./routes/category");
+const productRoutes=require("./routes/product")
+const orderRoutes=require("./routes/order")
+
+const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 8000;
+
+//DB connection
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => [console.log("DB IS CONNECTED")]);
+
+//middleware
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+//routes
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
+app.use("/api", orderRoutes);
+
+
+
+app.listen(port, () => {
+  console.log(`APP is running at ${port}`);
+});
